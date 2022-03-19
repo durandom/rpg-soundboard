aebef8475a4f7051afe9327bf99a9cca3ec5067d
Marcel Hild <hild@b4mad.net>
2022-03-19 21:51:01 +0100

    better mp3 detection

--- convert-mp3.py
+++ convert-mp3.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
+from re import I
 import sys
 import os
 import subprocess
+import pathlib
+import tempfile
+import shutil
 
 args = sys.argv[1:]
 
@@ -23,6 +27,8 @@ if os.path.exists(dst_basedir):
 
 os.mkdir(dst_basedir)
 dst_dir = dst_basedir
+tmp_directory = tempfile.mkdtemp()
+
 
 for dirpath, dirnames, filenames in os.walk(src_basedir):
     dir = dirpath.removeprefix(src_basedir)
@@ -36,14 +42,30 @@ for dirpath, dirnames, filenames in os.walk(src_basedir):
     for a_file in filenames:
         # lame -v -b 32 -B 128 -m m "$fullfile" "converted-mp3/${filename}.mp3"
         in_file = os.path.join(dirpath, a_file)
-        out_file = os.path.join(dst_dir, a_file)
+
+        ext = pathlib.Path(in_file).suffix.lower()
+        if ext != '.mp3':
+            if ext in ['.ogg', '.wav']:
+                print(f'convert to mp3: {a_file}')
+                tmp_out_file = os.path.join(tmp_directory, a_file + '.mp3')
+                subprocess.run(['sox', in_file, tmp_out_file])
+
+                stem = pathlib.Path(in_file).stem
+                out_file = os.path.join(dst_dir, stem + '.mp3')
+                in_file = tmp_out_file
+            else:
+                print(f'cant convert {in_file}')
+                continue
+        else:
+            out_file = os.path.join(dst_dir, a_file)
+
         # https://timothygu.me/lame/usage.html
         # https://svn.code.sf.net/p/lame/svn/trunk/lame/USAGE
         # https://www.stefaanlippens.net/audio_conversion_cheat_sheet/
         # subprocess.run(['lame', '-b 32', '-B 128', '-m','m', in_file, out_file])
         # subprocess.run(['lame', '--abr', '56', '-m','m', in_file, out_file])
         subprocess.run(['lame', '-V9', '-m','m', in_file, out_file])
+        print(f'converted {a_file}')
 
-        print(f'convert {a_file}')
 
-
+shutil.rmtree(tmp_directory)
